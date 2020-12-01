5bd2ae7364d0fc373ca59e473d116d15d98ed395
Samuel Merritt <65547748+smerritt3n2@users.noreply.github.com>
2020-12-01 16:06:18 +0000

    Delete email.php

--- email.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "vueadmin-3b1f69@inbox.mailtrap.io";
-$from = $user->document.getElementById('email');
-$subject = "My subject";
-$txt = $text->document.getElementById('message');
-$headers = "From: webmaster@example.com" . "\r\n" ."CC: somebodyelse@example.com";
-
-mail($to,$subject,$txt,$headers);
-?>
